096748d214155377d11da665e7a0cc9be5386535
Shayaan Siddiqui <shayaan@develop.io>
2018-09-01 11:27:36 -0400

    Created MainBody and Header component with place holder images and text

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import {Button} from 'reactstrap';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import MainBody from './components/MainBody/MainBody';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button color="danger">Danger!</Button>
+        <Header />
+        <MainBody />
       </div>
     );
   }
